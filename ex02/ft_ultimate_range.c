f1db041ae82d2aaab7f7ce5846b019c5349f7fb1
jsolano- <jsolano-@student.42madrid.com>
2021-04-26 12:48:09 +0200

    fixing norm

--- ex02/ft_ultimate_range.c
+++ ex02/ft_ultimate_range.c
@@ -1,6 +1,29 @@
 #include <stdlib.h>
 
- char *ft_strjoin(int size, char **strs, char *sep)
- {
-	 
- }
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	bound;
+	int	index;
+	int	*buffer;
+
+	if (min >= max)
+	{
+		*range = 0;
+		return (0);
+	}
+	bound = max - min - 1;
+	buffer = malloc(bound * sizeof(int));
+	if (buffer == NULL)
+	{
+		*range = 0;
+		return (-1);
+	}
+	*range = buffer;
+	index = 0;
+	while (index <= bound)
+	{
+		buffer[index] = min + index;
+		index++;
+	}
+	return (bound + 1);
+}
